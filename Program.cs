e21b0da1a0e89e9d752c70d457185e6e11127c97
Svitlana Andrieieva <svitlana0andrieieva@gmail.com>
2025-05-23 15:02:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] numbersForSort = new int[]
+{
+    4, 3, 2, 1, 7, 9, 5
+};
+
+for (int j = 0; j < numbersForSort.Length; j++)
+{
+    int max = numbersForSort[j];
+    int iterator = j;
+
+    for (int i = j; i < numbersForSort.Length; i++)
+    {
+        if (numbersForSort[i] > max)
+        {
+            max = numbersForSort[i];
+            iterator = i;
+        }
+    }
+
+    int buffer = numbersForSort[j];
+    numbersForSort[j] = max;
+    numbersForSort[iterator] = buffer;
+}
+
